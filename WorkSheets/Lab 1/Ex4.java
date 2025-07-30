224c09262736783217a9e4a08889c627e0ceef3c
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2025-07-30 08:36:01 +0530

    Add files via upload

---
+++ WorkSheets/Lab 1/Ex4.java
@@ -0,0 +1,11 @@
+public class Ex4 {
+    public static void main(String args[]) {
+		char ch = 'x';
+		int i = 456;
+		double d = 123.009;
+
+		System.out.println("ch is "  + ch);
+		System.out.println("i is " + i);
+		System.out.println("d is " + d);
+	}
+} 
