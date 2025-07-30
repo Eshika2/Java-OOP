a0414cd98db8ca6d216cca3027cab723677069b0
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2025-07-30 08:35:23 +0530

    Add files via upload

---
+++ WorkSheets/Lab 1/Ex3.java
@@ -0,0 +1,18 @@
+public class Ex3 {
+    
+    public static void main (String[] args)
+    {
+        int x = 15;
+        int y = 5;
+
+        int sum = x + y;
+        int sub = x - y;
+        int mul = x * y;
+        double div = x / y;
+
+        System.out.println ("Addition : " + sum);
+        System.out.println ("Subtraction : " + sub);
+        System.out.println ("Multiplication : " + mul);
+        System.out.println ("Division : " + div);
+    }
+}
