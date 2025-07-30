11ad697c9e2ce11d27e1a202764ba28263a2dc41
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2025-07-30 08:34:24 +0530

    Add files via upload

---
+++ WorkSheets/Lab 1/Ex2.java
@@ -0,0 +1,31 @@
+public class Ex2 {
+    
+    public static void main (String[] args)
+    {
+        System.out.println ("* * * *            *");
+        System.out.println ("* * * *           * *");
+        System.out.println ("* * * *          * * *");
+        System.out.println ("* * * *         * * * *");
+
+        System.out.println();
+
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i=1; i<=4; i++) {
+            for(int j=0; j<4; j++) {
+                System.out.print(" ");
+            }
+            for(int k=0; j<i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
