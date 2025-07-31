adcb50421115d0747af21401216da06067c7749b
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2025-07-31 12:35:49 +0530

    Add files via upload

---
+++ WorkSheets/Lab 1/Ex6.java
@@ -0,0 +1,40 @@
+public class Ex6 {
+    public static void main(String[] args) {
+        int i = 37;
+        int j = 42;
+        double x = 27.475;
+        double y = 7.22;
+
+        System.out.println("Variable values...");
+        System.out.println(" i = " + i );
+        System.out.println(" j = " + j );
+        System.out.println(" x = " + x );
+        System.out.println(" y = " + y );
+//Addition
+        System.out.println("Adding...");
+        System.out.println(" i + j = " + (i+j));
+        System.out.println(" x + y = " + (x+y));
+//Subtraction  
+        System.out.println("substracting...");
+	    System.out.println(" i - j = " + (i-j));
+	    System.out.println(" x - y = " + (x-y));
+//Multiplication	
+        System.out.println("multiplying...");
+        System.out.println(" i * j = " + (i*j));
+        System.out.println(" x * y = " + (x*y));
+//dividing numbers
+        System.out.println("dividing...");
+        System.out.println(" i / j = " + (i/j));
+        System.out.println(" x / y = " + (x/y));
+//the remainder resulting from dividing numbers
+        System.out.println("the remainder...");
+        System.out.println(" i % j = " + (i%j));
+        System.out.println(" x % y = " + (x%y));
+//mixing types
+        System.out.println("mixing types...");
+        System.out.println("  j + y = " + (j+y));
+        System.out.println("  i * x = " + (i*x));
+
+    }    
+
+}
