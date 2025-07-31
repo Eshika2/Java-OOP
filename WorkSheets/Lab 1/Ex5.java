db4095bed67f136e2c230ebdea0e26b646e8e424
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2025-07-31 12:35:22 +0530

    Add files via upload

---
+++ WorkSheets/Lab 1/Ex5.java
@@ -0,0 +1,15 @@
+public class Ex5 {
+    public static void main (String[] args)
+    {
+        double x = 15;
+        double y = 5;
+
+        double sum = x + y;
+        double avg = sum / 2;
+        double sum_square = ((x*x)+(y*y));
+
+        System.out.println ("Addition : " + sum);
+        System.out.println ("Average : " + avg);
+        System.out.println ("Sum of Square : " + sum_square);
+    }
+}
