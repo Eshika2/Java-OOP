75a4f48c4a12a3c814b5b0c3897478b5c9e18dd3
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2025-08-05 09:33:41 +0530

    Add files via upload

---
+++ WorkSheets/Lab 2/Task3.java
@@ -0,0 +1,15 @@
+public class Task3 {
+    public static void main (String[] args)
+    {
+        String name = args[0];
+        int age = Integer.parseInt(args[1]);
+
+        System.out.println ("My name is : " + name);
+        System.out.println ("My age is : " + age);
+
+    }
+}
+
+//java Task3 Eshika 20
+//My name is : Eshika
+//My age is : 20
