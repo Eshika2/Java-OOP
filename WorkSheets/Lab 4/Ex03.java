681411a46f89e8b9860c15628baf1b212964daa2
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2025-08-12 14:37:15 +0530

    Add files via upload

---
+++ WorkSheets/Lab 4/Ex03.java
@@ -0,0 +1,10 @@
+public class Ex03 {
+    public static void main (String[] args)
+    {
+        int num1 = Integer.parseInt (args[0]);
+        int num2 = Integer.parseInt (args[1]);
+
+        int max = (num1 < num2) ? num2 : num1;
+        System.out.println ("Maximum number is : " + max);
+    }
+}
