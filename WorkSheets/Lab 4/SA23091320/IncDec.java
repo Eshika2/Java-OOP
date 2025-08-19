57655859d43c8570a47cb6abe978c7aa09cd15d1
Eshika Dissanayake <151606601+Eshika2@users.noreply.github.com>
2025-08-19 09:03:28 +0530

    Add files via upload

---
+++ WorkSheets/Lab 4/SA23091320/IncDec.java
@@ -0,0 +1,16 @@
+public class IncDec{
+	public static void main(String args[ ]) {
+
+		int a = 1;
+		int b = 2;
+		int c = ++b; //c=(b=b+1)  c=3
+		int d = a++; //d=a
+		c++;
+        // c=4
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+	}
+}
